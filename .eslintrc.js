0e7a78a9202424cb326b4a866ce3ae7304c34312
Nadim Tuhin <naidmtuhin@gmail.com>
2017-01-11 09:20:28 +0600

    no named as default rules ignored

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
     "import"
   ],
   "rules": {
+    "import/no-named-as-default": 0,
+    "import/no-named-as-default-member": 0,
     "react/jsx-filename-extension": [1, {"extensions": [".js", ".jsx"]}],
   },
   "env": {
